5588b3051c3e358eac8e052c869a98e227818eba
Adrian Tesula <adrian.tesula@ivfuture.uk>
2020-10-15 19:03:20 +0300

    Second deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Work in progress
+          Work in progress for RINESTONE.
       </header>
     </div>
   );
