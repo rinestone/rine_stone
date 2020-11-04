42e63acfe4a59cc568ae00d7e3d90be5fcce58a4
Adrian T <adrian97gl@yahoo.ro>
2020-11-04 22:00:01 +0200

    Updates

--- precache-manifest.741886787d944663224c829a117301ab.js
+++ precache-manifest.741886787d944663224c829a117301ab.js
@@ -1,31 +1,39 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cf3a60abf1e3404d4746c763a0cf182",
+    "revision": "a622d61099cf47253bf1266efb4ef46e",
     "url": "/rine_stone/index.html"
   },
   {
-    "revision": "18b7dadfa200a0cc135d",
-    "url": "/rine_stone/static/css/main.b05506aa.chunk.css"
+    "revision": "bbabdb0492fed99c9e9e",
+    "url": "/rine_stone/static/css/main.c93ce409.chunk.css"
   },
   {
-    "revision": "5d88103696da06afe8dc",
-    "url": "/rine_stone/static/js/2.c853324b.chunk.js"
+    "revision": "161ca79f84b6a9afee3d",
+    "url": "/rine_stone/static/js/2.4ed6cd84.chunk.js"
   },
   {
     "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
-    "url": "/rine_stone/static/js/2.c853324b.chunk.js.LICENSE.txt"
+    "url": "/rine_stone/static/js/2.4ed6cd84.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "18b7dadfa200a0cc135d",
-    "url": "/rine_stone/static/js/main.2f9fe7a4.chunk.js"
+    "revision": "bbabdb0492fed99c9e9e",
+    "url": "/rine_stone/static/js/main.82d715f1.chunk.js"
   },
   {
     "revision": "813d9bec092dee13187e",
     "url": "/rine_stone/static/js/runtime-main.08e5526d.js"
   },
   {
+    "revision": "ccd68127d316130965098d54e4d7ba15",
+    "url": "/rine_stone/static/media/homeFlowers.ccd68127.png"
+  },
+  {
     "revision": "8eac8de5e1a2772c3c95e56e9d17cb55",
     "url": "/rine_stone/static/media/slide1.8eac8de5.png"
+  },
+  {
+    "revision": "8a2a1a59accb72d53242cbd1cbfa1bfc",
+    "url": "/rine_stone/static/media/slide3.8a2a1a59.png"
   },
   {
     "revision": "a5db8a2db789a5fb78c4044e00a3635b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rine_stone/precache-manifest.a82587a2698e566973a6a63bf94a7d76.js"
+  "/rine_stone/precache-manifest.741886787d944663224c829a117301ab.js"
 );
 
 self.addEventListener('message', (event) => {
